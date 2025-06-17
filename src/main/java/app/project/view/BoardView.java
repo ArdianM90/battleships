7d143397653a9297e3c8ec5b85007269eb452ee8
Adrian Mieńkowski <amienkowski@gmail.com>
2025-06-17 09:45:20 +0200

    Board JButtons changed to JPanels

--- src/main/java/app/project/view/BoardView.java
+++ src/main/java/app/project/view/BoardView.java
@@ -4,8 +4,11 @@ import app.project.model.BoardType;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.function.*;
 
+import static app.project.model.BoardType.FOE_BOARD;
 import static app.project.model.BoardType.SETUP_BOARD;
 
 public class BoardView extends JPanel {
@@ -13,7 +16,7 @@ public class BoardView extends JPanel {
     private static final int BUTTON_SIZE = 35;
 
     private final BoardType boardType;
-    private final JButton[][] rectsArr;
+    private final ShipTile[][] shipsArr;
     private final BiConsumer<BoardType, Point> notifyClickFunction;
     private final BiPredicate<BoardType, Point> isShipFunction;
 
@@ -23,49 +26,42 @@ public class BoardView extends JPanel {
         this.notifyClickFunction = notifyClickFunction;
         this.isShipFunction = isShipFunction;
         this.boardType = boardType;
-
-        this.rectsArr = new JButton[size][size];
+        this.shipsArr = new ShipTile[size][size];
         setLayout(new GridLayout(size, size, 1, 1));
         setPreferredSize(new Dimension((BUTTON_SIZE + 1) * size, (BUTTON_SIZE + 1) * size));
 
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
-                this.rectsArr[row][col] = createButton(new Point(row, col));
-                add(this.rectsArr[row][col]);
+                this.shipsArr[row][col] = createTile(new Point(row, col));
+                add(this.shipsArr[row][col]);
             }
         }
     }
 
-    private JButton createButton(Point point) {
-        // todo: przenieść sterowanie kolorem do kontrolera
-        JButton button = new JButton();
-        button.setBackground(isShipFunction.test(boardType, point) ? Color.RED : Color.BLUE);
-        button.setOpaque(true);
-        button.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-        button.setPreferredSize(new Dimension(BUTTON_SIZE, BUTTON_SIZE));
-
-        button.addActionListener(_ -> shipOnClick(point));
-        return button;
+    private ShipTile createTile(Point point) {
+        ShipTile tile = new ShipTile();
+        tile.setBackground(isShipFunction.test(boardType, point) ? Color.RED : Color.BLUE);
+        tile.setOpaque(true);
+        tile.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        tile.setPreferredSize(new Dimension(BUTTON_SIZE, BUTTON_SIZE));
+        tile.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (SETUP_BOARD.equals(boardType) || FOE_BOARD.equals(boardType)) {
+                    notifyClickFunction.accept(boardType, point);
+                }
+            }
+        });
+        return tile;
     }
 
-    private void shipOnClick(Point point) {
-        if (SETUP_BOARD.equals(boardType)) {
-            notifyClickFunction.accept(boardType, point);
-        } else if (BoardType.FOE_BOARD.equals(boardType)) {
-            notifyClickFunction.accept(boardType, point);
-        }
-    }
-
-    public void markShip(Point point) {
-        JButton button = rectsArr[point.x][point.y];
-        Color newColor = isShipFunction.test(boardType, point) ? Color.RED : Color.BLUE;
-        button.setBackground(newColor);
+    public void toggleShip(Point point) {
+        shipsArr[point.x][point.y].setShip(isShipFunction.test(boardType, point));
+        shipsArr[point.x][point.y].repaint();
     }
 
     public void drawShot(Point point) {
-        JButton button = rectsArr[point.x][point.y];
-        button.setText("X");
-        button.setFont(new Font("Arial", Font.BOLD, 20));
-        button.setForeground(Color.WHITE);
+        shipsArr[point.x][point.y].markShot();
+        shipsArr[point.x][point.y].repaint();
     }
 }
--- src/main/java/app/project/view/ShipsSetupPanel.java
+++ src/main/java/app/project/view/ShipsSetupPanel.java
@@ -51,6 +51,6 @@ public class ShipsSetupPanel extends JPanel {
     }
 
     private void handleSetupBoardClick(Point point) {
-        boardView.markShip(point);
+        boardView.toggleShip(point);
     }
 }
