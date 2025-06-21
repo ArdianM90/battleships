2d2a467d91decb05cb24b00ddd79bf6f4fb97785
Adrian Mieńkowski <amienkowski@gmail.com>
2025-06-21 13:11:17 +0200

    second Battlesips insance removed

--- src/main/java/app/project/Main.java
+++ src/main/java/app/project/Main.java
@@ -7,8 +7,6 @@ public class Main {
         if (args.length > 0 && args[0].equals("--test")) {
             testMode = true;
         }
-        System.out.println("Test mode: " + testMode);
-        new Battleships(testMode);
         new Battleships(testMode);
     }
 }
